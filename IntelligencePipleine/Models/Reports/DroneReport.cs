9b5865d6d1fd55703d695f07cb09bfb21a770a9c
Mordy Pesach <mordypassover@gmail.com>
2026-06-30 16:57:59 +0300

    added basic DroneReport code

--- IntelligencePipleine/Models/Reports/DroneReport.cs
+++ IntelligencePipleine/Models/Reports/DroneReport.cs
@@ -2,8 +2,24 @@ using System;
 
 namespace IntelligencePipeline.Models.Reports
 {
-    class droneReport: Report
+    class DroneReport: Report
     {
+        private int _altitude;
+        private int _imageQuality;
 
+        int Altitude { get; set; } 
+        int ImageQuality { get; set; }
+
+        public DroneReport(int reportId, DateTime timestamp, double latitude, double longitude, string description,
+            int altitude, int imageQuality) : base(reportId, timestamp, latitude, longitude, description)
+        {
+            Altitude = altitude;
+            ImageQuality = imageQuality;
+        }
+        public override string GetSourceType() => "Drone";
+        public override int CalculateReliabilityScore()
+        {
+            return 0;
+        }
     }
 }
