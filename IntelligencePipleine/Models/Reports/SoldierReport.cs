e589fb8f4ce907e475bacabad26127beac0a8319
Mordy Pesach <mordypassover@gmail.com>
2026-06-30 17:20:08 +0300

    added basic SoldierReport code

--- IntelligencePipleine/Models/Reports/SoldierReport.cs
+++ IntelligencePipleine/Models/Reports/SoldierReport.cs
@@ -4,6 +4,29 @@ namespace IntelligencePipeline.Models.Reports
 {
     class SoldierReport : Report
     {
+        private string _soldierName;
+        private string _soldierID;
+        private string _unit;
+        private int _confidenceLevel;
 
+        string SoldierName { get; set; }
+        string SoldierID { get; set; } 
+        string Unit { get; set; } 
+        int ConfidenceLevel { get; set; }
+
+        public SoldierReport(int reportId, DateTime timestamp, double latitude, double longitude,
+            string description, string soldierName, string soldierID, string unit, int confidenceLevel) 
+            : base(reportId, timestamp, latitude, longitude, description)
+        {
+            SoldierName = soldierName;
+            SoldierID = soldierID;
+            Unit = unit;
+            ConfidenceLevel = confidenceLevel;
+        }
+        public override string GetSourceType() => "Soldier";
+        public override int CalculateReliabilityScore()
+        {
+            return 0;
+        }
     }
 }
