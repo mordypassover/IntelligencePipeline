ed7840bef6e07fb76f2b5409dff48b48e0a96f78
Mordy Pesach <mordypassover@gmail.com>
2026-06-30 16:43:34 +0300

    added basic Report class code

--- IntelligencePipleine/Models/Reports/Report.cs
+++ IntelligencePipleine/Models/Reports/Report.cs
@@ -1,9 +1,54 @@
+using IntelligencePipeline.Models.Enums;
 using System;
 
 namespace IntelligencePipeline.Models.Reports
 {
-    class Report
+    abstract class Report
     {
+        private int _reportId;
+        private DateTime _timestamp;
+        private double _latitude;
+        private double _longitude;
+        private string _description;
+        private ReportStatus _status;
+        private Priority _priority;
+        private Classification _classification;
+        private int _reliabilityScore;
+        private string? _rejectionReason;
+
+        int ReportId { get; } 
+        DateTime Timestamp { get; set; } 
+        double Latitude { get; set; } 
+        double Longitude { get; set; } 
+        string Description { get; set; } 
+        ReportStatus Status { get; set; } 
+        Priority Priority { get; set; }
+        Classification Classification { get; set; }
+        int ReliabilityScore { get; set; }
+        string RejectionReason { get; set; }
+
+        protected Report(int reportId, DateTime timestamp, double latitude,double longitude, string description)
+        {
+            ReportId = reportId;
+            Timestamp = timestamp;
+            Latitude = latitude;
+            Longitude = longitude;
+            Description = description;
+            Status = ReportStatus.New;
+        }
+        public abstract string GetSourceType();
+        public abstract int CalculateReliabilityScore();
+
+        public virtual string GetSummary()
+        {
+            return $" ReportId = {ReportId} Timestamp = {Timestamp} Latitude = {Latitude}" +
+                   $" Longitude = {Longitude} Description = {Description} Status = {Status}";
+        } 
+        public override string ToString()
+        {
+            return $" ReportId = {ReportId} Timestamp = {Timestamp} Latitude = {Latitude}" +
+                   $" Longitude = {Longitude} Description = {Description} Status = {Status}";
+        }
 
     }
 }
