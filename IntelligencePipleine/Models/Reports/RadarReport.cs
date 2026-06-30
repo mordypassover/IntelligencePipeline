9d6d21d6a498fdb66a57a163c513c3e1907aaef4
Mordy Pesach <mordypassover@gmail.com>
2026-06-30 17:20:40 +0300

    added basic RadarReport code

--- IntelligencePipleine/Models/Reports/RadarReport.cs
+++ IntelligencePipleine/Models/Reports/RadarReport.cs
@@ -4,6 +4,27 @@ namespace IntelligencePipeline.Models.Reports
 {
     class RadarReport : Report
     {
+        private int _speed;
+        private int _direction;
+        private int _distance;
 
+        int Speed { get; set; } 
+        int Direction { get; set; }
+        int Distance { get; set; }
+
+        public RadarReport(int reportId, DateTime timestamp, double latitude,
+        double longitude, string description, int speed, int direction, int distance)
+        : base(reportId, timestamp, latitude, longitude, description)
+        {
+            Speed = speed;
+            Direction = direction;
+            Distance = distance;
+        }
+        public override string GetSourceType() => "Radar";
+
+        public override int CalculateReliabilityScore()
+        {
+            return 0;
+        }
     }
 }
