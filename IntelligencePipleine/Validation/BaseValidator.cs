0d633cf4c0468e4f22e28977919b8db78bc7a4fb
Mordy Pesach <mordypassover@gmail.com>
2026-07-01 15:37:29 +0300

    added base validator class code

--- IntelligencePipleine/Validation/BaseValidator.cs
+++ IntelligencePipleine/Validation/BaseValidator.cs
@@ -7,17 +7,29 @@ namespace IntelligencePipeline.Validation
     {
         public ValidationResult Validate(Report report)
         {
-            ValidationResult commonFieldsAreValidation = ValidateCommonFields(report);
-            if (commonFieldsAreValidation.IsValid) 
+            // checks common fieldes then if success checks specific validator
+            ValidationResult commonFieldsAreValid = ValidateCommonFields(report);
+            if (commonFieldsAreValid.IsValid) 
             {
                 return ValidateSpecificFields(report);
             }
-            return commonFieldsAreValidation;
+            return commonFieldsAreValid;
 
         }
         protected ValidationResult ValidateCommonFields(Report report)
         {
-            // nead to add validations! 
+            List<string>? messages = new List<string>(); // colects all validation messeges 
+
+            if (report.Timestamp.Year < 2020 || report.Timestamp.Year > 2026) { messages.Add("timestamp not valid") ; }
+            if( report.Latitude < 29.5000 || report.Latitude > 33.5000) { messages.Add("Latitude not valid"); }
+            if (report.Longitude < 34.0000 || report.Longitude > 36.0000) { messages.Add("Longitude not valid"); }
+            if (report.Description.Length < 10 || report.Description.Length > 500 ) { messages.Add("Description not valid"); }
+
+            if (messages != null) 
+            {
+                return ValidationResult.Failure(string.Join(", ", messages));
+            }
+        
             return ValidationResult.Success();
         }
         protected abstract ValidationResult ValidateSpecificFields(Report report);
