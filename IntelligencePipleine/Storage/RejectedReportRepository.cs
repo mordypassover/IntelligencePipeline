34c4b6d05c0bc5fe35f79868ab78311048c143de
Mordy Pesach <mordypassover@gmail.com>
2026-07-02 09:38:52 +0300

    added Rejected Report repository class

--- IntelligencePipleine/Storage/RejectedReportRepository.cs
+++ IntelligencePipleine/Storage/RejectedReportRepository.cs
@@ -20,13 +20,27 @@ namespace IntelligencePipeline.Storage
         }
         public void Add(Report report)
         {
-
+            RejectedReport.Add(report);
         }
         public List<Report> GetAll() => RejectedReport;
 
         public int GetTotalCount() => RejectedReport.Count();
 
-        public List<Report> GetByReason(string reasonKeyword) { }
+        public List<Report> GetByReason(string reasonKeyword) 
+        {
+            List<Report> reportsWithReason = new List<Report>();
+            foreach ( Report report in RejectedReport)
+            {
+                foreach (string reason in report.RejectionReason.Split(','))
+                {
+                    if (reason == reasonKeyword)
+                    {
+                        reportsWithReason.Add(report);
+                        break;
+                    }
+                }
+            }
+            return reportsWithReason;
+        }
     }
-
 }
