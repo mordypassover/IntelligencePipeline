3e9c27ee91b713a51d19cd02f7c3c81cacfb93e8
Mordy Pesach <mordypassover@gmail.com>
2026-07-02 12:41:00 +0300

    updated Reliability Calculator to validate

--- IntelligencePipleine/Calculators/ReliabilityCalculator.cs
+++ IntelligencePipleine/Calculators/ReliabilityCalculator.cs
@@ -7,7 +7,13 @@ namespace IntelligencePipeline.Calculators
     {
         public int Calculate(Report report)
         {
-            return report.CalculateReliabilityScore();
+            int reliability = report.CalculateReliabilityScore();
+
+            //checks if reliability is valid and sets to 1 if not
+
+            if (reliability < 1 || reliability > 10) { return 1;} 
+
+            return reliability;
         }
     }
 }
